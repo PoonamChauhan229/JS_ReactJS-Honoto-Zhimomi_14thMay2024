4e367152e499b15793cc346c3b9b9cc28eeae384
PoonamChauhan229 <poonam.chauhan229@gmail.com>
2024-11-21 17:27:54 +0530

    recat props

--- React/02DayReact/src/App.jsx
+++ React/02DayReact/src/App.jsx
@@ -13,16 +13,12 @@ function App() {
     {/* <LearningDisplay/> */}
     <div className='profileSection'>
       {/* name img email city */}
-      <Profile name="Poonam" email={"poonam@gmail.com"} city={"Mumbai"}/>
+      <Profile name="Poonam" email={"poonam@gmail.com"} city={"Mumbai"} birthDate="19thJan"/>
       <Profile name="Honoto" email={"honoto@gmail.com"} city={"Shillong"}/>
       <Profile name="Alice" email={"alice89@gmail.com"}
       city="Phoenix, AR"/>
-      <Profile/>
-      <Profile/>
-      <Profile/>
-      <Profile/>
-      <Profile/>
-      <Profile/>
+      <Profile phoneNumber="678987654"/>
+      
     </div>
    </React.Fragment>
   )
--- React/02DayReact/src/Components/Profile.jsx
+++ React/02DayReact/src/Components/Profile.jsx
@@ -8,19 +8,41 @@ function Profile(props){
                 <img src="https://img.freepik.com/free-vector/young-man-with-glasses-illustration_1308-174706.jpg?ga=GA1.1.700783662.1728457916&semt=ais_hybrid" alt="" />
                 <p>Email: {props.email ||"TestEMail"}</p>
                 <p>City: {props.city ||"TestCity"}</p>
+                <p>BirthDate:{props.birthDate||"21stJan"}</p>
+                {/* <p>PhoneNumber:{props.phoneNumber}</p> */}
+                {/* if phoneNumber >>phonenumber  */}
+                {
+                    // ternary 
+                    //(1==1)?"React":"No React"
+                    (props.phoneNumber)?<p>PhoneNumber:{props.phoneNumber}</p>:null                  
+
+                }     
+                {
+                     (props.birthDate)?<p>BirthDate:{props.birthDate}</p>:null
+                }
+                 {
+                     (props.birthDate)?<p>BirthDate:{props.birthDate}</p>:"BirthDate:21stjan"
+                }
             </div>
+          
 
 
-            <div className="Profile">
+            {/* <div className="Profile">
                 <h3>Name: Alice</h3>
                 <h4>Email: alice89@gmail.com</h4>
                 <h4>City: Phoenix, AR</h4>
-            </div>
+            </div> */}
             
         </>
     )
 }
 export default Profile
+
+// Profile >> 6-7 Profile 
+// Conditionally tried to uitilize the data
+// >> || OR operator
+// >>  ternary opertor 
+//props passing 
 
 // Style 
 // NOrmal Style 
@@ -29,3 +51,4 @@ export default Profile
 // class >> REact >> stateful >> class component
 // class reserve keyword >> CSS
 // className="" >> rule >> remains same
+
